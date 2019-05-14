94e71449c7104deff9d18d62cfdf7ee757d58d81
knewjade <knewjade@gmail.com>
2019-05-14 23:47:46 +0900

    Found prioritize solutions

--- src/finder/perfect.cpp
+++ src/finder/perfect.cpp
@@ -17,25 +17,46 @@ namespace finder {
 
             return sum % 4 == 0;
         }
+
+        bool shouldUpdate(const Record &oldRecord, const Record &newRecord) {
+            if (newRecord.softdropCount != oldRecord.softdropCount) {
+                return newRecord.softdropCount < oldRecord.softdropCount;
+            }
+
+            if (newRecord.lineClearCount != oldRecord.lineClearCount) {
+                return newRecord.lineClearCount < oldRecord.lineClearCount;
+            }
+
+            return newRecord.holdCount < oldRecord.holdCount;
+        }
+
+        bool isWorseThanBest(const Record &best, const Candidate &current) {
+            return best.softdropCount < current.softdropCount || INT_MAX < current.lineClearCount;
+        }
     }
 
     template<>
-    bool PerfectFinder<core::srs::MoveGenerator>::move(
-            Configure &configure,
+    void PerfectFinder<core::srs::MoveGenerator>::move(
+            const Configure &configure,
             const Candidate &candidate,
             Solution &solution,
             std::vector<core::Move> &moves,
             core::PieceType pieceType,
             int nextIndex,
-            int nextHoldIndex
+            int nextHoldIndex,
+            int nextHoldCount
     );
 
     template<>
-    bool PerfectFinder<core::srs::MoveGenerator>::search(
-            Configure &configure,
+    void PerfectFinder<core::srs::MoveGenerator>::search(
+            const Configure &configure,
             const Candidate &candidate,
             Solution &solution
     ) {
+        if (isWorseThanBest(best, candidate)) {
+            return;
+        }
+
         auto depth = candidate.depth;
 
         auto pieces = configure.pieces;
@@ -46,15 +67,15 @@ namespace finder {
         auto holdIndex = candidate.holdIndex;
         assert(-1 <= holdIndex && holdIndex < configure.pieceSize);
 
+        auto holdCount = candidate.holdCount;
+
         bool canUseCurrent = currentIndex < configure.pieceSize;
         if (canUseCurrent) {
             assert(currentIndex < pieces.size());
             auto &current = pieces[currentIndex];
 
             moves.clear();
-            if (move(configure, candidate, solution, moves, current, currentIndex + 1, holdIndex)) {
-                return true;
-            }
+            move(configure, candidate, solution, moves, current, currentIndex + 1, holdIndex, holdCount);
         }
 
         if (0 <= holdIndex) {
@@ -65,9 +86,7 @@ namespace finder {
                 auto &hold = pieces[holdIndex];
 
                 moves.clear();
-                if (move(configure, candidate, solution, moves, hold, currentIndex + 1, currentIndex)) {
-                    return true;
-                }
+                move(configure, candidate, solution, moves, hold, currentIndex + 1, currentIndex, holdCount + 1);
             }
         } else {
             assert(canUseCurrent);
@@ -81,24 +100,33 @@ namespace finder {
                 auto &next = pieces[nextIndex];
 
                 moves.clear();
-                if (move(configure, candidate, solution, moves, next, nextIndex + 1, currentIndex)) {
-                    return true;
-                }
+                move(configure, candidate, solution, moves, next, nextIndex + 1, currentIndex, holdCount + 1);
             }
         }
-
-        return false;
     }
 
     template<>
-    bool PerfectFinder<core::srs::MoveGenerator>::move(
-            Configure &configure,
+    void PerfectFinder<core::srs::MoveGenerator>::accept(const Record& record) {
+        assert(1 <= best.solution.size());
+
+        if (best.solution[0].x == -1 || shouldUpdate(best, record)) {
+            best.solution = record.solution;
+            best.softdropCount = record.softdropCount;
+            best.holdCount = record.holdCount;
+            best.lineClearCount = record.lineClearCount;
+        }
+    }
+    
+    template<>
+    void PerfectFinder<core::srs::MoveGenerator>::move(
+            const Configure &configure,
             const Candidate &candidate,
             Solution &solution,
             std::vector<core::Move> &moves,
             core::PieceType pieceType,
             int nextIndex,
-            int nextHoldIndex
+            int nextHoldIndex,
+            int nextHoldCount
     ) {
         auto depth = candidate.depth;
         auto maxDepth = configure.maxDepth;
@@ -106,6 +134,9 @@ namespace finder {
 
         auto leftLine = candidate.leftLine;
         assert(0 < leftLine);
+
+        auto softdropCount = candidate.softdropCount;
+        auto lineClearCount = candidate.lineClearCount;
 
         moveGenerator.search(moves, field, pieceType, leftLine);
 
@@ -117,13 +148,19 @@ namespace finder {
 
             int numCleared = freeze.clearLineReturnNum();
 
+            solution[depth].pieceType = pieceType;
+            solution[depth].rotateType = move.rotateType;
+            solution[depth].x = move.x;
+            solution[depth].y = move.y;
+
+            int nextSoftdropCount = move.harddrop ? softdropCount : softdropCount + 1;
+            int nextLineClearCount = 0 < numCleared ? lineClearCount + 1 : lineClearCount;
+
             int nextLeftLine = leftLine - numCleared;
             if (nextLeftLine == 0) {
-                solution[depth].pieceType = pieceType;
-                solution[depth].rotateType = move.rotateType;
-                solution[depth].x = move.x;
-                solution[depth].y = move.y;
-                return true;
+                auto record = Record{solution, nextSoftdropCount, nextHoldCount, nextLineClearCount};
+                accept(record);
+                return;
             }
 
             auto nextDepth = depth + 1;
@@ -135,17 +172,12 @@ namespace finder {
                 continue;
             }
 
-            Candidate nextCandidate = Candidate{freeze, nextIndex, nextHoldIndex, nextLeftLine, nextDepth};
-            if (search(configure, nextCandidate, solution)) {
-                solution[depth].pieceType = pieceType;
-                solution[depth].rotateType = move.rotateType;
-                solution[depth].x = move.x;
-                solution[depth].y = move.y;
-                return true;
-            }
+            Candidate nextCandidate = Candidate{
+                    freeze, nextIndex, nextHoldIndex, nextLeftLine, nextDepth,
+                    nextSoftdropCount, nextHoldCount, nextLineClearCount
+            };
+            search(configure, nextCandidate, solution);
         }
-
-        return false;
     }
 
     template<>
@@ -153,30 +185,49 @@ namespace finder {
             const core::Field &field, const std::vector<core::PieceType> &pieces,
             int maxDepth, int maxLine, bool holdEmpty
     ) {
+        assert(1 <= maxDepth);
+
+        // Copy field
         auto freeze = core::Field(field);
 
+        // Initialize moves
         std::vector<std::vector<core::Move>> movePool(maxDepth);
-        Solution solution(maxDepth);
-        for (int i = 0; i < maxDepth; ++i) {
-            movePool[i] = std::vector<core::Move>{};
-            solution[i] = Operation{};
+        for (int index = 0; index < maxDepth; ++index) {
+            movePool[index] = std::vector<core::Move>{};
         }
 
-        Configure configure{
+        // Initialize solution
+        Solution solution(maxDepth);
+        for (int index = 0; index < maxDepth; ++index) {
+            solution[index] = Operation{
+                    core::PieceType::T, core::RotateType::Spawn, -1, -1
+            };
+        }
+
+        // Initialize configure
+        const Configure configure{
                 pieces,
                 movePool,
                 maxDepth,
                 static_cast<int>(pieces.size()),
         };
 
+        // Create candidate
         Candidate candidate = holdEmpty
-                              ? Candidate{freeze, 0, -1, maxLine, 0}
-                              : Candidate{freeze, 1, 0, maxLine, 0};
+                              ? Candidate{freeze, 0, -1, maxLine, 0, 0, 0, 0}
+                              : Candidate{freeze, 1, 0, maxLine, 0, 0, 0, 0};
 
-        if (search(configure, candidate, solution)) {
-            return solution;
-        } else {
-            return kNoSolution;
-        }
+        // Create current record & best record
+        best = Record{
+                std::vector(solution),
+                INT_MAX,
+                INT_MAX,
+                INT_MAX,
+        };
+
+        // Execute
+        search(configure, candidate, solution);
+
+        return best.solution[0].x == -1 ? kNoSolution : std::vector<Operation>(best.solution);
     }
 }
