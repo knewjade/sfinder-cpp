3ec36afe6a45be5961478f50ed46753ae5ac08ed
knewjade <knewjade@gmail.com>
2019-06-09 00:04:57 +0900

    Add reachable

--- test/core/test_moves.cpp
+++ test/core/test_moves.cpp
@@ -6,139 +6,241 @@
 namespace core {
     using namespace std::literals::string_literals;
 
-    class MovesTest : public ::testing::Test {
-    };
-
     bool assertMove(std::vector<Move> &moves, const Move &move) {
         auto result = std::find(moves.begin(), moves.end(), move);
         return result != moves.end();
     }
 
-    TEST_F(MovesTest, case1) {
-        auto field = createField(
-                "XXXX____XX"s +
-                "XXXX___XXX"s +
-                "XXXX__XXXX"s +
-                "XXXX___XXX"s +
-                ""
-        );
+    namespace harddrop {
+        class HarddropMoveGeneratorTest : public ::testing::Test {
+        };
 
-        auto factory = Factory::create();
-        auto generator = srs::MoveGenerator(factory);
+        TEST_F(HarddropMoveGeneratorTest, case1) {
+            auto field = createField(
+                    "XXXX____XX"s +
+                    "XXXX___XXX"s +
+                    "XXXX__XXXX"s +
+                    "XXXX___XXX"s +
+                    ""
+            );
 
-        {
-            auto moves = std::vector<Move>();
-            generator.search(moves, field, PieceType::T, 4);
+            auto factory = Factory::create();
+            auto generator = harddrop::MoveGenerator(factory);
 
-            EXPECT_EQ(moves.size(), 7);
-            EXPECT_TRUE(assertMove(moves, Move{RotateType::Spawn, 5, 0, false}));
-            EXPECT_TRUE(assertMove(moves, Move{RotateType::Right, 4, 1, true}));
-            EXPECT_TRUE(assertMove(moves, Move{RotateType::Right, 5, 2, true}));
-            EXPECT_TRUE(assertMove(moves, Move{RotateType::Reverse, 5, 2, true}));
-            EXPECT_TRUE(assertMove(moves, Move{RotateType::Reverse, 6, 3, true}));
-            EXPECT_TRUE(assertMove(moves, Move{RotateType::Left, 5, 1, true}));
+            {
+                auto moves = std::vector<Move>();
+                generator.search(moves, field, PieceType::T, 4);
+
+                EXPECT_EQ(moves.size(), 6);
+                EXPECT_TRUE(assertMove(moves, Move{RotateType::Right, 4, 1, true}));
+                EXPECT_TRUE(assertMove(moves, Move{RotateType::Right, 5, 2, true}));
+                EXPECT_TRUE(assertMove(moves, Move{RotateType::Reverse, 5, 2, true}));
+                EXPECT_TRUE(assertMove(moves, Move{RotateType::Reverse, 6, 3, true}));
+                EXPECT_TRUE(assertMove(moves, Move{RotateType::Left, 5, 1, true}));
+            }
+
+            {
+                auto moves = std::vector<Move>();
+                generator.search(moves, field, PieceType::S, 4);
+
+                EXPECT_EQ(moves.size(), 3);
+                EXPECT_TRUE(assertMove(moves, Move{RotateType::Spawn, 5, 1, true}));
+                EXPECT_TRUE(assertMove(moves, Move{RotateType::Spawn, 6, 2, true}));
+                EXPECT_TRUE(assertMove(moves, Move{RotateType::Left, 5, 1, true}));
+            }
         }
 
-        {
-            auto moves = std::vector<Move>();
-            generator.search(moves, field, PieceType::S, 4);
+    }
 
-            EXPECT_EQ(moves.size(), 3);
-            EXPECT_TRUE(assertMove(moves, Move{RotateType::Spawn, 5, 1, true}));
-            EXPECT_TRUE(assertMove(moves, Move{RotateType::Spawn, 6, 2, true}));
-            EXPECT_TRUE(assertMove(moves, Move{RotateType::Left, 5, 1, true}));
+    namespace srs {
+        class SRSMoveGeneratorTest : public ::testing::Test {
+        };
+
+        TEST_F(SRSMoveGeneratorTest, case1) {
+            auto field = createField(
+                    "XXXX____XX"s +
+                    "XXXX___XXX"s +
+                    "XXXX__XXXX"s +
+                    "XXXX___XXX"s +
+                    ""
+            );
+
+            auto factory = Factory::create();
+            auto generator = srs::MoveGenerator(factory);
+
+            {
+                auto moves = std::vector<Move>();
+                generator.search(moves, field, PieceType::T, 4);
+
+                EXPECT_EQ(moves.size(), 7);
+                EXPECT_TRUE(assertMove(moves, Move{RotateType::Spawn, 5, 0, false}));
+                EXPECT_TRUE(assertMove(moves, Move{RotateType::Right, 4, 1, true}));
+                EXPECT_TRUE(assertMove(moves, Move{RotateType::Right, 5, 2, true}));
+                EXPECT_TRUE(assertMove(moves, Move{RotateType::Reverse, 5, 2, true}));
+                EXPECT_TRUE(assertMove(moves, Move{RotateType::Reverse, 6, 3, true}));
+                EXPECT_TRUE(assertMove(moves, Move{RotateType::Left, 5, 1, true}));
+            }
+
+            {
+                auto moves = std::vector<Move>();
+                generator.search(moves, field, PieceType::S, 4);
+
+                EXPECT_EQ(moves.size(), 3);
+                EXPECT_TRUE(assertMove(moves, Move{RotateType::Spawn, 5, 1, true}));
+                EXPECT_TRUE(assertMove(moves, Move{RotateType::Spawn, 6, 2, true}));
+                EXPECT_TRUE(assertMove(moves, Move{RotateType::Left, 5, 1, true}));
+            }
+        }
+
+        TEST_F(SRSMoveGeneratorTest, case2) {
+            auto field = createField(
+                    "XXXXXX__XX"s +
+                    "XXXXX__XXX"s +
+                    ""
+            );
+
+            auto factory = Factory::create();
+            auto generator = srs::MoveGenerator(factory);
+
+            {
+                auto moves = std::vector<Move>();
+                generator.search(moves, field, PieceType::S, 2);
+
+                EXPECT_EQ(moves.size(), 1);
+                EXPECT_TRUE(assertMove(moves, Move{RotateType::Spawn, 6, 0, false}));
+            }
+        }
+
+        TEST_F(SRSMoveGeneratorTest, case3) {
+            auto field = createField(
+                    "XXXX_X__XX"s +
+                    "XXXX__XXXX"s +
+                    "XXXX___XXX"s +
+                    ""
+            );
+
+            auto factory = Factory::create();
+            auto generator = srs::MoveGenerator(factory);
+
+            {
+                auto moves = std::vector<Move>();
+                generator.search(moves, field, PieceType::S, 3);
+
+                EXPECT_EQ(moves.size(), 0);
+            }
+        }
+
+        TEST_F(SRSMoveGeneratorTest, case4) {
+            auto field = createField(
+                    "_XXXXXXXXX"s +
+                    "__XXXXXXXX"s +
+                    "X_XXXXXXXX"s +
+                    ""
+            );
+
+            auto factory = Factory::create();
+            auto generator = srs::MoveGenerator(factory);
+
+            {
+                auto moves = std::vector<Move>();
+                generator.search(moves, field, PieceType::S, 3);
+
+                EXPECT_EQ(moves.size(), 0);
+            }
         }
     }
 
-    TEST_F(MovesTest, harddrop1) {
-        auto field = createField(
-                "XXXX____XX"s +
-                "XXXX___XXX"s +
-                "XXXX__XXXX"s +
-                "XXXX___XXX"s +
-                ""
-        );
+    namespace srs_rotate_end {
+        class SRSRotateEndReachableTest : public ::testing::Test {
+        };
 
-        auto factory = Factory::create();
-        auto generator = harddrop::MoveGenerator(factory);
+        TEST_F(SRSRotateEndReachableTest, case1) {
+            auto factory = Factory::create();
+            auto reachable = srs_rotate_end::Reachable(factory);
 
-        {
-            auto moves = std::vector<Move>();
-            generator.search(moves, field, PieceType::T, 4);
+            {
+                auto field = createField(
+                        "XXXX__XXXX"s +
+                        "XXXXX__XXX"s +
+                        ""
+                );
 
-            EXPECT_EQ(moves.size(), 6);
-            EXPECT_TRUE(assertMove(moves, Move{RotateType::Right, 4, 1, true}));
-            EXPECT_TRUE(assertMove(moves, Move{RotateType::Right, 5, 2, true}));
-            EXPECT_TRUE(assertMove(moves, Move{RotateType::Reverse, 5, 2, true}));
-            EXPECT_TRUE(assertMove(moves, Move{RotateType::Reverse, 6, 3, true}));
-            EXPECT_TRUE(assertMove(moves, Move{RotateType::Left, 5, 1, true}));
-        }
+                EXPECT_EQ(reachable.checks(field, PieceType::Z, RotateType::Spawn, 5, 0, 24), RotateType::Reverse);
+                EXPECT_EQ(reachable.checks(field, PieceType::Z, RotateType::Reverse, 5, 1, 24), RotateType::Reverse);
+            }
 
-        {
-            auto moves = std::vector<Move>();
-            generator.search(moves, field, PieceType::S, 4);
+            {
+                auto field = createField(
+                        "XX________"s +
+                        "X_________"s +
+                        "X_XXXXXXXX"s +
+                        "X_XXXXXXXX"s +
+                        "X_XXXXXXXX"s +
+                        ""
+                );
 
-            EXPECT_EQ(moves.size(), 3);
-            EXPECT_TRUE(assertMove(moves, Move{RotateType::Spawn, 5, 1, true}));
-            EXPECT_TRUE(assertMove(moves, Move{RotateType::Spawn, 6, 2, true}));
-            EXPECT_TRUE(assertMove(moves, Move{RotateType::Left, 5, 1, true}));
-        }
-    }
+                EXPECT_EQ(reachable.checks(field, PieceType::I, RotateType::Left, 1, 1, 24), RotateType::Right);
+                EXPECT_EQ(reachable.checks(field, PieceType::I, RotateType::Right, 1, 2, 24), RotateType::Right);
+            }
 
-    TEST_F(MovesTest, case2) {
-        auto field = createField(
-                "XXXXXX__XX"s +
-                "XXXXX__XXX"s +
-                ""
-        );
+            {
+                auto field = createField(
+                        "XX________"s +
+                        "X_________"s +
+                        "X_X_XXXXXX"s +
+                        "X_X_XXXXXX"s +
+                        "X_XXXXXXXX"s +
+                        ""
+                );
 
-        auto factory = Factory::create();
-        auto generator = srs::MoveGenerator(factory);
+                EXPECT_EQ(reachable.checks(field, PieceType::I, RotateType::Left, 1, 1, 24), kUnreachable);
+                EXPECT_EQ(reachable.checks(field, PieceType::I, RotateType::Right, 1, 2, 24), kUnreachable);
+            }
 
-        {
-            auto moves = std::vector<Move>();
-            generator.search(moves, field, PieceType::S, 2);
+            {
+                auto field = createField(
+                        "XXX_______"s +
+                        "XXX_______"s +
+                        "XXX_XXXXXX"s +
+                        "XXX____XXX"s +
+                        ""
+                );
 
-            EXPECT_EQ(moves.size(), 1);
-            EXPECT_TRUE(assertMove(moves, Move{RotateType::Spawn, 6, 0, false}));
-        }
-    }
+                EXPECT_EQ(reachable.checks(field, PieceType::I, RotateType::Reverse, 5, 0, 24), RotateType::Reverse);
+                EXPECT_EQ(reachable.checks(field, PieceType::I, RotateType::Spawn, 4, 0, 24), RotateType::Reverse);
+            }
 
-    TEST_F(MovesTest, case3) {
-        auto field = createField(
-                "XXXX_X__XX"s +
-                "XXXX__XXXX"s +
-                "XXXX___XXX"s +
-                ""
-        );
+            {
+                auto field = createField(
+                        "__________"s +
+                        "XXX_______"s +
+                        "X____XXXXX"s +
+                        "XXX____XXX"s +
+                        ""
+                );
 
-        auto factory = Factory::create();
-        auto generator = srs::MoveGenerator(factory);
+                EXPECT_EQ(reachable.checks(field, PieceType::I, RotateType::Reverse, 3, 1, 24), RotateType::Reverse);
+                EXPECT_EQ(reachable.checks(field, PieceType::I, RotateType::Spawn, 2, 1, 24), RotateType::Reverse);
 
-        {
-            auto moves = std::vector<Move>();
-            generator.search(moves, field, PieceType::S, 3);
+                EXPECT_EQ(reachable.checks(field, PieceType::I, RotateType::Reverse, 5, 0, 24), kUnreachable);
+                EXPECT_EQ(reachable.checks(field, PieceType::I, RotateType::Spawn, 4, 0, 24), kUnreachable);
+            }
 
-            EXPECT_EQ(moves.size(), 0);
-        }
-    }
+            {
+                auto field = createField(
+                        "X__XXXXXXX"s +
+                        "X___XXXXXX"s +
+                        "XX__XXXXXX"s +
+                        ""
+                );
 
-    TEST_F(MovesTest, case4) {
-        auto field = createField(
-                "_XXXXXXXXX"s +
-                "__XXXXXXXX"s +
-                "X_XXXXXXXX"s +
-                ""
-        );
+                EXPECT_EQ(reachable.checks(field, PieceType::O, RotateType::Spawn, 1, 1, 24), RotateType::Spawn);
 
-        auto factory = Factory::create();
-        auto generator = srs::MoveGenerator(factory);
-
-        {
-            auto moves = std::vector<Move>();
-            generator.search(moves, field, PieceType::S, 3);
-
-            EXPECT_EQ(moves.size(), 0);
+                EXPECT_EQ(reachable.checks(field, PieceType::O, RotateType::Spawn, 2, 0, 24), kUnreachable);
+                EXPECT_EQ(reachable.checks(field, PieceType::O, RotateType::Right, 2, 1, 24), kUnreachable);
+                EXPECT_EQ(reachable.checks(field, PieceType::O, RotateType::Reverse, 3, 1, 24), kUnreachable);
+                EXPECT_EQ(reachable.checks(field, PieceType::O, RotateType::Left, 3, 0, 24), kUnreachable);
+            }
         }
     }
 }
