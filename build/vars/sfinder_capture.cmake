ae57e839a938cf908c3d4a12d3ba27b1427c11d0
knewjade <knewjade@gmail.com>
2019-06-14 19:18:28 +0900

    Organize files

---
+++ build/vars/sfinder_capture.cmake
@@ -0,0 +1,6 @@
+# File defines
+set(sfinder_capture_SOURCE_DIR ${CMAKE_SOURCE_DIR}/capture)
+
+file(GLOB sfinder_capture_SRC
+        ${sfinder_capture_SOURCE_DIR}/main.cpp
+        )
---
+++ build/vars/sfinder_core.cmake
@@ -0,0 +1,7 @@
+# File defines
+set(sfinder_core_SOURCE_DIR ${CMAKE_SOURCE_DIR}/src)
+
+file(GLOB sfinder_core_SRC
+        ${sfinder_core_SOURCE_DIR}/*/*.cpp
+        ${sfinder_core_SOURCE_DIR}/*/*.hpp
+        )
