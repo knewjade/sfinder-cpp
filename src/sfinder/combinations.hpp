2aa58e9d69d7994173af5ed9cb8f11d0b7b29a9f
knewjade <knewjade@gmail.com>
2019-12-07 23:36:42 +0900

    Change definition place

--- src/sfinder/combinations.hpp
+++ src/sfinder/combinations.hpp
@@ -5,63 +5,6 @@
 
 namespace sfinder {
     template<typename Container, typename Op>
-    void combination(const Container &container, int pop, Op op) {
-        using valueType = typename Container::value_type;
-
-        const int size = container.size();
-
-        if (pop == size) {
-            auto elements = std::vector<valueType>(container.cbegin(), container.cend());
-            op(elements);
-            return;
-        }
-
-        if (size - pop < pop) {
-            combinationReverse(container, size - pop, op);
-            return;
-        }
-
-        auto indexes = std::vector<int>(pop);
-        auto elements = std::vector<valueType>(pop);
-
-        for (int i = 0; i < pop; ++i) {
-            int selectedIndex = pop - i - 1;
-            indexes[i] = selectedIndex;
-            elements[i] = container[selectedIndex];
-        }
-
-        op(elements);
-
-        while (true) {
-            int i = 0;
-            for (i = 0; i < pop; ++i) {
-                auto nextIndex = indexes[i] + 1;
-                if (size - i <= nextIndex) {
-                    continue;
-                }
-
-                // Next state decided
-
-                indexes[i] = nextIndex;
-                elements[i] = container[nextIndex];
-
-                for (int j = i - 1; 0 <= j; --j) {
-                    int selectedIndex = indexes[j + 1] + 1;
-                    indexes[j] = selectedIndex;
-                    elements[j] = container[selectedIndex];
-                }
-
-                op(elements);
-                break;
-            }
-
-            if (i == pop) {
-                break;
-            }
-        }
-    }
-
-    template<typename Container, typename Op>
     void combinationReverse(const Container &container, int pop, Op op) {
         using valueType = typename Container::value_type;
 
@@ -128,6 +71,63 @@ namespace sfinder {
                         elements[nextIndex] = container[k];
                         nextIndex += 1;
                     }
+                }
+
+                op(elements);
+                break;
+            }
+
+            if (i == pop) {
+                break;
+            }
+        }
+    }
+
+    template<typename Container, typename Op>
+    void combination(const Container &container, int pop, Op op) {
+        using valueType = typename Container::value_type;
+
+        const int size = container.size();
+
+        if (pop == size) {
+            auto elements = std::vector<valueType>(container.cbegin(), container.cend());
+            op(elements);
+            return;
+        }
+
+        if (size - pop < pop) {
+            combinationReverse(container, size - pop, op);
+            return;
+        }
+
+        auto indexes = std::vector<int>(pop);
+        auto elements = std::vector<valueType>(pop);
+
+        for (int i = 0; i < pop; ++i) {
+            int selectedIndex = pop - i - 1;
+            indexes[i] = selectedIndex;
+            elements[i] = container[selectedIndex];
+        }
+
+        op(elements);
+
+        while (true) {
+            int i = 0;
+            for (i = 0; i < pop; ++i) {
+                auto nextIndex = indexes[i] + 1;
+                if (size - i <= nextIndex) {
+                    continue;
+                }
+
+                // Next state decided
+
+                indexes[i] = nextIndex;
+                elements[i] = container[nextIndex];
+
+                for (int j = i - 1; 0 <= j; --j) {
+                    int selectedIndex = indexes[j + 1] + 1;
+                    indexes[j] = selectedIndex;
+                    elements[j] = container[selectedIndex];
                 }
 
                 op(elements);
